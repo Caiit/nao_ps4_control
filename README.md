159ceeccba9c609c4c16e52ac1228181021add3c
Caiit <caitlin.lagrand@live.nl>
2019-09-16 16:00:56 +0200

    Python PS4 control for Nao robot.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# nao_ps4_control
-Control the NAO robot with a PS4 controller.
+# NAO PS4 Control
+
+## Requirements
+- python 2
+- [pygame](https://www.pygame.org/news)
+- [NaoQi](http://doc.aldebaran.com/2-1/dev/python/install_guide.html)
+- [Pillow](https://pypi.org/project/Pillow/)
+
+
+TODO: check if sound files need to be copied to the robot
+
+## Controls
+Left joystick: move in x/y direction
+Right joystick: turn
+X: wave
+O: kick
+
+
+
+## Add new motions
+- Create a new python file in `motions/`.
+- Add the motion to the NAO class.
+- Add the motion to a specific button in the PS4 class.
+
+
+
+## C++
+Install smfl
+`sudo apt-get install libsfml-dev`
+
---
+++ scripts/nao.py
@@ -0,0 +1,53 @@
+import math
+from naoqi import ALProxy
+
+# Motions
+from motions import wave, kick, elephant, saxophone, picture
+
+
+class NAO(object):
+    def __init__(self, ip, port):
+        print("Hi I am Nao")
+
+        # Proxies
+        self.motion_proxy = ALProxy("ALMotion", ip, port)
+        self.posture_proxy = ALProxy("ALRobotPosture", ip, port)
+        self.text_to_speech_proxy = ALProxy("ALTextToSpeech", ip, port)
+        self.audio_player_proxy = ALProxy("ALAudioPlayer", ip, port)
+        self.camera_proxy = ALProxy("ALVideoDevice", ip, port)
+
+        # Last walking commands
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+
+    def walk(self, x, y, theta):
+        if (self.x != 0 or self.y != 0 or self.theta != 0) and (x == 0 and y == 0 and theta == 0):
+            # If not already stopped, but need to stop
+            self.motion_proxy.stopMove()
+            self.posture_proxy.goToPosture("StandInit", 0.5)
+        else:
+            self.motion_proxy.moveToward(round(-y, 2), round(-x, 2), round(theta / math.pi, 2))
+        self.x = x
+        self.y = y
+        self.theta = theta
+
+    def wave(self):
+        wave.wave(self.motion_proxy, self.text_to_speech_proxy)
+        self.posture_proxy.goToPosture("StandInit", 0.5)
+
+    def kick(self):
+        kick.kick(self.motion_proxy)
+        self.posture_proxy.goToPosture("StandInit", 0.5)
+
+    def elephant(self):
+        elephant.elephant(self.motion_proxy, self.audio_player_proxy)
+        self.posture_proxy.goToPosture("StandInit", 0.5)
+
+    def saxophone(self):
+        saxophone.saxophone(self.motion_proxy, self.audio_player_proxy)
+        self.posture_proxy.goToPosture("StandInit", 0.5)
+
+    def take_picture(self):
+        picture.take_picture(self.motion_proxy, self.audio_player_proxy, self.camera_proxy)
+        self.posture_proxy.goToPosture("StandInit", 0.5)
